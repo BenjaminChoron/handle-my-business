064eae1a4384208206dd9dd48a53429d70169c42
BenjCH <benjamin.choron@outlook.com>
2025-02-19 19:29:20 +0100

    🗃️ Create product table migration and postman collection with product routes

---
+++ src/database/migrations/1739986236983-CreateProductsTable.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateProductsTable1739986236983 implements MigrationInterface {
+  name = 'CreateProductsTable1739986236983';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "products" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying NOT NULL, "price" numeric NOT NULL, "stock" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0806c755e0aca124e67c0cf6d7d" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "products"`);
+  }
+}
