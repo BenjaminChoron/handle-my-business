548044d5fbfc630b23d1381ed5deeeb3b41ebf11
BenjCH <benjamin.choron@outlook.com>
2025-02-19 19:06:52 +0100

    🔧 Add database configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT', 3000);
+
+  await app.listen(process.env.APP_PORT ?? 3000);
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
 
 void bootstrap();
