98aad8d93464bfd9662e92dc7c262d9b175e5b30
BenjCH <benjamin.choron@outlook.com>
2025-02-25 00:19:52 +0100

    🔧 Simplify Prettier configuration in package scripts and CI workflow

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  // Remove the problematic import order options
+}; 
