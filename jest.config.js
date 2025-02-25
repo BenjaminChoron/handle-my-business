88504a5991fea8e9080beae414d6918aa3e738ae
BenjCH <benjamin.choron@outlook.com>
2025-02-25 01:22:50 +0100

    🔧 Update project configuration and dependencies
    - Upgrade NestJS and related dependencies to latest versions
    - Add Prettier import sorting plugin
    - Update Jest and TypeScript configurations
    - Enhance GitHub Actions workflow with comprehensive quality checks
    - Refactor package scripts for improved development workflow

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,25 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)s$': ['ts-jest', {
-      tsconfig: 'tsconfig.json',
-      isolatedModules: true,
-    }],
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/*.module.ts',
+    '!**/main.ts',
+    '!**/migrations/*.ts'
+  ],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  collectCoverage: true,
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
     '^@app/(.*)$': '<rootDir>/src/modules/$1',
   },
   testPathIgnorePatterns: ['/node_modules/'],
-}; 
+};
